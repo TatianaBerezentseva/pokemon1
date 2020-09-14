53ab92c90e1ba1c2de00fa66fab0f21c504448b0
redpanda <vivalatanya@gmail.com>
2020-09-14 15:17:39 +0500

    fix ошибки

--- pokemon1.py
+++ pokemon1.py
@@ -5,18 +5,16 @@ def main():
 
     structure = library['results']
 
-    min = structure[0].get('name')
-    max = structure[0].get('name')
+    minName = structure[0].get('name')
+    maxName = structure[0].get('name')
 
     for i in range(len(structure)):
-        iteration = structure[i].get('name')
-        if len(min) > iteration:
-            min = iteration
-        elif len(max) < iteration:
-            max = iteration
-    print("Самое короткое имя " + min)
-    print("Cамое длинное имя " + max)
-
-
+        name = structure[i].get('name')
+        if len(minName) > len(name):
+            minName = name
+        elif len(maxName) < len(name):
+            maxName = name
+    print("Самое короткое имя " + minName)
+    print("Cамое длинное имя " + maxName)
 
 main()
