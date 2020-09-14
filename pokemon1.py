75a93b2116c82e6483ee5c19d4381701622b0292
redpanda <vivalatanya@gmail.com>
2020-09-14 14:46:28 +0500

    finish work

--- pokemon1.py
+++ pokemon1.py
@@ -1,6 +1,21 @@
 import requests
 def main():
-    r = requests.get('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200')
-    print("Hello world")
-    print(r)
+    response = requests.get('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200')
+    library = response.json()
+
+    structure = library['results']
+
+    min = structure[0].get('name')
+    max = structure[0].get('name')
+
+    for i in range(len(structure)):
+        if len(min) > len(structure[i].get('name')):
+            min = structure[i].get('name')
+        elif len(max) < len(structure[i].get('name')):
+            max = structure[i].get('name')
+    print("Самое короткое имя " + min)
+    print("Cамое длинное имя " + max)
+
+
+
 main()
